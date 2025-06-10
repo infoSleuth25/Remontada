b6b1f63c8a7622e880f677d29c83784b64030531
Siddhesh <siddheshborse25@gmail.com>
2025-06-10 12:28:18 +0530

    Login and register frontend pages are completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React,{lazy} from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+const Home = lazy(()=> import('./pages/Home'));
+const Login = lazy(()=> import('./pages/Login'));
+const Chat = lazy(()=> import('./pages/Chat'));
+const Groups = lazy(()=> import('./pages/Groups'));
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/groups' element={<Groups />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
