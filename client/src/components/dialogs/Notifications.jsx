970b14a651815e3777e62e35d7493d25c88059b7
Siddhesh <siddheshborse25@gmail.com>
2025-06-12 17:57:40 +0530

    Notifications Dialog is added

--- client/src/components/dialogs/Notifications.jsx
+++ client/src/components/dialogs/Notifications.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { Avatar, Button, Dialog, DialogTitle, ListItem, Stack, Typography } from '@mui/material'
+import { memo } from 'react'
+import { sampleNotification } from '../../constants/sampleData'
 
 const Notifications = () => {
+  const friendRequestHandler = ({_id,accept}) =>{
+
+  }
   return (
-    <div>Notifications</div>
+    <Dialog open>
+      <Stack p={"2rem"} maxWidth={"25rem"}>
+        <DialogTitle>Notifications</DialogTitle>
+        {
+          sampleNotification.length > 0 ? 
+          (
+            sampleNotification.map((i)=> <NotificationItem sender={i.sender} _id={i._id} handler={friendRequestHandler} key={i._id} />)
+          ) : 
+          (<Typography textAlign={"center"}>0 Notifications</Typography>)
+        }
+      </Stack>
+    </Dialog>
   )
 }
+
+const NotificationItem = memo(({sender, _id, handler}) =>{
+  const {name,avatar} = sender;
+  return (
+    <ListItem>
+        <Stack direction={"row"} alignItems={"center"} spacing={"1rem"} width={"100%"}>
+            <Avatar src={avatar} />
+            <Typography 
+                variant='body1' 
+                sx={{
+                    flexGrow:1,
+                    display:"-webkit-box",
+                    WebkitLineClamp:1,
+                    WebkitBoxOrient:"vertical",
+                    overflow : "hidden",
+                    textOverflow : "ellipsis",
+                    width : "100%"
+                }}>{`${name} sent you a friend request.`}
+            </Typography>
+            <Stack direction={"row"} >
+              <Button sx={{color:"green"}} onClick={()=>handler({_id,accept:true})}>Accept</Button>
+              <Button color="error" onClick={()=>handler({_id,accept:false})}>Reject</Button>
+            </Stack>
+            
+        </Stack>
+    </ListItem>
+  )
+}) 
 
 export default Notifications
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -17,13 +17,30 @@ export const sampleChats = [
 
 export const sampleUsers = [
     {
-        avatar : ["https://www.w3schools.com/howto/img_avatar.png"],
+        avatar : "https://www.w3schools.com/howto/img_avatar.png",
         name : "John Doe",
         _id : "1",
     },
     {
-        avatar : ["https://www.w3schools.com/howto/img_avatar.png"],
+        avatar : "https://www.w3schools.com/howto/img_avatar.png",
         name : "John Boi",
+        _id : "2",
+    }
+]
+
+export const sampleNotification = [
+    {
+        sender:{
+            avatar : "https://www.w3schools.com/howto/img_avatar.png",
+            name : "John Doe"
+        },
+        _id : "1",
+    },
+    {
+        sender:{
+            avatar : "https://www.w3schools.com/howto/img_avatar.png",
+            name : "John Boi",
+        },
         _id : "2",
     }
 ]
