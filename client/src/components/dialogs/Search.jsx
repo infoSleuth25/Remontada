e2200482ffa8d32e78595fe709a7ce3bc139ac4d
Siddhesh <siddheshborse25@gmail.com>
2025-06-12 10:55:18 +0530

    Search Dialog is added

--- client/src/components/dialogs/Search.jsx
+++ client/src/components/dialogs/Search.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { Dialog, DialogTitle, InputAdornment, List, ListItem, ListItemText, Stack, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import {Search as SearchIcon} from '@mui/icons-material'
+import {useInputValidation} from '6pp'
+import UserItem from '../shared/UserItem';
+import {sampleUsers} from '../../constants/sampleData'
+
 
 const Search = () => {
+  const search = useInputValidation("");
+  const addFriendHandler = (id) =>{
+    console.log(id);
+  }
+  let isLoadingSendFriendRequest = false;
+  const [users, setUsers] = useState(sampleUsers); 
   return (
-    <div>Search</div>
+    <Dialog open>
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment :(
+              <InputAdornment position='start'>
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }} 
+        />
+        <List>
+          {
+            users.map((i)=>(
+              <UserItem user={i} ket={i._id} handler={addFriendHandler} handleIsLoading={isLoadingSendFriendRequest} />
+            ))
+          }
+        </List>
+      </Stack>
+    </Dialog>
   )
 }
 
